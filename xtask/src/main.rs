b65f086403c7d90406cfa8107efb27d060211621
Callum Tolley <cgtrolley@gmail.com>
2025-04-20 11:31:51 +0100

    Add xtask run

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,7 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
+use std::ffi::OsStr;
+use std::fmt::Display;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
@@ -21,9 +23,21 @@ enum Commands {
         #[arg(default_value = "pack")]
         pack_dir: PathBuf,
     },
+    /// Run the monitor service
+    Run {
+        /// Whether to run the release binary
+        #[arg(short, long, default_value = "false")]
+        release: bool,
+    },
+    /// Run the monitor service and watch for changes
+    Watch {
+        /// Whether to run the release binary
+        #[arg(short, long, default_value = "false")]
+        release: bool,
+    },
 }
 
-fn print_cargo_style(message: &str, action: &str) {
+fn print_cargo_style(action: impl Display, message: impl Display) {
     println!("{}{:>12} {}{}", GREEN_BOLD, action, RESET, message);
 }
 
@@ -54,10 +68,20 @@ fn copy_file(src: &Path, dest: &Path) -> Result<()> {
     let dest_dir = dest_normalized.parent().unwrap_or(&dest_normalized);
 
     print_cargo_style(
-        &format!("`{}` to `{}`", src_normalized.display(), dest_dir.display()),
         "Copying",
+        &format!("`{}` to `{}`", src_normalized.display(), dest_dir.display()),
     );
-    std::fs::copy(src, dest)?;
+    if let Some(dest_parent) = dest_normalized.parent() {
+        std::fs::create_dir_all(dest_parent)
+            .with_context(|| format!("Failed to create directory `{}`", dest_parent.display()))?;
+    }
+    std::fs::copy(src, dest).with_context(|| {
+        format!(
+            "Failed to copy `{}` to `{}`",
+            src_normalized.display(),
+            dest_normalized.display()
+        )
+    })?;
     Ok(())
 }
 
@@ -67,8 +91,8 @@ fn copy_dir(src: &Path, dest: &Path) -> Result<()> {
     let dest_dir = dest_normalized.parent().unwrap_or(&dest_normalized);
 
     print_cargo_style(
-        &format!("`{}` to `{}`", src_normalized.display(), dest_dir.display()),
         "Copying",
+        &format!("`{}` to `{}`", src_normalized.display(), dest_dir.display()),
     );
 
     copy_dir_silent(src, dest)?;
@@ -84,7 +108,13 @@ fn copy_dir_silent(src: &Path, dest: &Path) -> Result<()> {
         if src_path.is_dir() {
             copy_dir_silent(&src_path, &dest_path)?;
         } else {
-            std::fs::copy(&src_path, &dest_path)?;
+            std::fs::copy(&src_path, &dest_path).with_context(|| {
+                format!(
+                    "Failed to copy `{}` to `{}`",
+                    src_path.display(),
+                    dest_path.display()
+                )
+            })?;
         }
     }
     Ok(())
@@ -97,8 +127,8 @@ fn main() -> Result<()> {
         Commands::Pack { pack_dir } => {
             // Create target directory if it doesn't exist
             print_cargo_style(
-                &format!("to `{}`", normalize_path(&pack_dir).display()),
                 "Packaging",
+                &format!("to `{}`", normalize_path(&pack_dir).display()),
             );
             if pack_dir.exists() {
                 std::fs::remove_dir_all(&pack_dir)?;
@@ -106,24 +136,10 @@ fn main() -> Result<()> {
             std::fs::create_dir_all(&pack_dir)?;
 
             // Build the release binary
-            let cargo_path = env!("CARGO");
-            print_cargo_style("hagias-monitor-service", "Building");
-            let status = Command::new(cargo_path)
-                .args(["build", "--release"])
-                .status()?;
-
-            if !status.success() {
-                anyhow::bail!("Failed to build release binary");
-            }
+            run_command(env!("CARGO"), ["build", "--release"])?;
 
             // Build the CSS
-            let status = Command::new("npm")
-                .args(["run", "build-release:css"])
-                .status()?;
-
-            if !status.success() {
-                anyhow::bail!("Failed to build CSS");
-            }
+            run_command("npm.cmd", ["run", "build-release:css"])?;
 
             // Get the workspace root
             let workspace_root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("..");
@@ -149,9 +165,69 @@ fn main() -> Result<()> {
                 copy_dir(&src_path, &target_path)?;
             }
 
-            print_cargo_style("package", "Finished");
+            print_cargo_style("Finished", "packaging");
+        }
+        Commands::Run { release } => {
+            // Build the CSS
+            let npm_build_css_script_name = if release {
+                "build-release:css"
+            } else {
+                "build:css"
+            };
+            run_command("npm.cmd", ["run", npm_build_css_script_name])?;
+
+            // Run the monitor service
+            let cargo_path = env!("CARGO");
+            let args = if release {
+                vec!["run", "--release"]
+            } else {
+                vec!["run"]
+            };
+            run_command(cargo_path, args)?;
+        }
+        Commands::Watch { release } => {
+            todo!()
         }
     }
 
     Ok(())
+}
+
+fn run_command<S1, S2, I>(command: S1, args: I) -> Result<(), anyhow::Error>
+where
+    S1: AsRef<OsStr>,
+    S2: AsRef<OsStr>,
+    I: IntoIterator<Item = S2>,
+{
+    let command = command.as_ref();
+    let command_normalized = normalize_path(&Path::new(command));
+    let args = args
+        .into_iter()
+        .map(|s| s.as_ref().to_owned())
+        .collect::<Vec<_>>();
+    let command_full = format!(
+        "{} {}",
+        command_normalized.display(),
+        args.iter()
+            .map(|arg| arg.display().to_string())
+            .map(|arg| if arg.contains(" ") {
+                format!("\"{}\"", arg)
+            } else {
+                arg
+            })
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
+    print_cargo_style("Running", &command_full);
+    let status = Command::new(&command)
+        .args(&args)
+        .status()
+        .with_context(|| format!("Failed to start command: {}", &command_full))?;
+    Ok(if !status.success() {
+        anyhow::bail!(
+            "Command returned exit code {}: {}",
+            status.code().unwrap_or(-1),
+            &command_full
+        );
+    })
 }
